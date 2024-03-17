c5be0a66f437aa4857bcb68bd427704a4ea362dc
Alex Goldman <90787028+Goldmaa@users.noreply.github.com>
2024-03-17 10:57:30 +0000

    Task 1 complete

--- task1.py
+++ task1.py
@@ -3,6 +3,7 @@
 import streamlit as st
 from st_pages import show_pages_from_config
 import pandas as pd
+import altair as alt
 
 from math import sin, cos, radians, sqrt
 
@@ -53,8 +54,8 @@ height = st.number_input(
 datapoints = st.number_input(
     label = "Number of datapoints",
     min_value = 1,
-    max_value = 1000,
-    value = 100
+    max_value = 5000,
+    value = 1000
 ) # determines how many points to calculate
 
 # Downards direction is negative, input is positive
@@ -71,36 +72,64 @@ u_y = launch_speed * sin_theta
 
 t_max = - ((u_y + sqrt((u_y ** 2) - (2 * g_strength * height))) / g_strength) # Own equation
 
-st.write(f"Determined time of flight: {t_max} seconds")
+st.markdown(
+    f"Determined time of flight (t<sub>max</sub>): {t_max} seconds",
+    unsafe_allow_html=True
+)
 
 time_increment = t_max / (datapoints - 1)
 
-st.write(f"Determined increment of time: {time_increment} seconds")
+st.write(f"Determined increment of time (Δt): {time_increment} seconds")
 
 t = pd.Series(
     [(time_increment * i) for i in range(datapoints)]
-    )
-
-t
+)
 
 # The rest is done using the provided equations, modified for a negative value of g
 
 x_pos = pd.Series(
-    [u_x * t_i for t_i in t]
-    )
+    [round(u_x, 14) * t_i for t_i in t]
+)
 
 y_pos = pd.Series(
     [height + (u_y * t_i) + (0.5 * g_strength * (t_i ** 2)) for t_i in t]
-    )
+)
 
 v_x = pd.Series(
     [u_x for _ in t]
-    )
+)
 
 v_y = pd.Series(
     [u_y + (g_strength * t_i) for t_i in t]
-    )
+)
 
 v = pd.Series(
     [sqrt((v_x_i ** 2) + (v_y_i ** 2)) for v_x_i, v_y_i in zip(v_x, v_y)]
-    )
+)
+
+pos = pd.DataFrame(
+    {
+        "Time (s)": t,
+        "x / m": x_pos,
+        "y / m": y_pos,
+        "X Velocity (m/s)": v_x,
+        "Y Velocity (m/s)": v_y,
+        "Velocity (m/s)": v
+    }
+)
+
+chart = (
+    alt.Chart(pos)
+    .mark_circle()
+    .encode(
+        x="x / m",
+        y="y / m",
+        color="Velocity (m/s)",
+        tooltip=[
+            "Time (s)", "x / m", "y / m", "X Velocity (m/s)", "Y Velocity (m/s)", "Velocity (m/s)"
+            ]
+        )
+    .resolve_scale(x="shared", y="shared")
+)
+
+st.altair_chart(chart, use_container_width=True)
