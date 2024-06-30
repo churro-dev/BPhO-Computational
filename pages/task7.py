25b2fb6b4b800d6250f860f2e8c249b0948c6b01
Sprocketer <90787028+Sprocketer@users.noreply.github.com>
2024-06-30 14:04:45 +0000

    Finished Task 7 part 1

--- pages/task7.py
+++ pages/task7.py
@@ -1,5 +1,5 @@
 # Imports
-from math import asin, cos, degrees, radians, sin, sqrt, tan, log
+from math import asin, cos, degrees, radians, sin, sqrt, tan
 
 import altair as alt
 import pandas as pd
@@ -39,7 +39,7 @@ datapoints = st.number_input(
 )  # determines how many points to calculate
 
 angles = st_tags(
-    label='# Enter Keywords:',
+    label='Angles (type & press **enter** to add more)',
     text='Press enter to add more',
     value=['30', '45', '60', '70.5', '78', '85'],
     maxtags = 148,
@@ -49,7 +49,7 @@ theta_degs = []
 for angle in angles:
     try:
         theta_degs.append(float(angle))
-    except:
+    except ValueError:
         pass
 
 # Downards direction is negative, input is positive
@@ -97,22 +97,30 @@ y_pos_Rs_dict = {f"y_pos_R_{i} / m": y_pos_R for i, y_pos_R in enumerate(y_pos_R
 t_R = pd.Series([x_pos_R_i / (u * cos_theta_R) for x_pos_R_i in x_pos_R])
 
 
-pos = pd.DataFrame(
-    {**{
-        "t_R / s": t_R,
-    },
-     **x_pos_Rs_dict,
-     **y_pos_Rs_dict
-    }
-)
-
-
 theme = st_theme()
 if theme != None:
     if theme['base'] == 'light':
-        colors = ['black', 'silver', 'gray', 'maroon', 'red', 'purple', 'fuchsia', 'green', 'lime', 'olive', 'yellow', 'navy', 'blue', 'teal', 'aqua', 'orange', 'aliceblue', 'aquamarine', 'azure', 'beige', 'bisque', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'forestgreen', 'gainsboro', 'gold', 'goldenrod', 'greenyellow', 'grey', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'limegreen', 'linen', 'magenta', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'oldlace', 'olivedrab', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'yellowgreen', 'rebeccapurple']
+        colors = ['black', 'silver', 'gray', 'maroon', 'red', 'purple', 'fuchsia', 'green', 'lime', 'olive', 'yellow', 'navy', 'blue', 'teal', 'aqua', 'orange', 'aquamarine', 'azure', 'beige', 'bisque', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'forestgreen', 'gainsboro', 'gold', 'goldenrod', 'greenyellow', 'grey', 'hotpink', 'indianred', 'indigo', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'limegreen', 'linen', 'magenta', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'midnightblue', 'moccasin', 'oldlace', 'olivedrab', 'orangered', 'orchid', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'sienna', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'yellowgreen', 'rebeccapurple']
     else:
         colors = ['silver', 'gray', 'white', 'maroon', 'red', 'purple', 'fuchsia', 'green', 'lime', 'olive', 'yellow', 'navy', 'blue', 'teal', 'aqua', 'orange', 'aliceblue', 'antiquewhite', 'aquamarine', 'azure', 'beige', 'bisque', 'blanchedalmond', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'greenyellow', 'grey', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightgrey', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'limegreen', 'linen', 'magenta', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'oldlace', 'olivedrab', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'whitesmoke', 'yellowgreen', 'rebeccapurple']
+       
+
+    for i, line in enumerate(list(x_pos_Rs_dict)):
+        new_key = f"x_{str(theta_degs[i])}".replace(".", "․")
+        x_pos_Rs_dict[new_key] = x_pos_Rs_dict.pop(line)
+    
+    for i, line in enumerate(list(y_pos_Rs_dict)):
+        new_key = f"y_{str(theta_degs[i])}".replace(".", "․")
+        y_pos_Rs_dict[new_key] = y_pos_Rs_dict.pop(line)
+    
+    pos = pd.DataFrame(
+        {**{
+            "t_R / s": t_R,
+        },
+        **x_pos_Rs_dict,
+        **y_pos_Rs_dict
+        }
+    )
 
     n = len(x_pos_Rs)
     if n != 0:
@@ -120,26 +128,29 @@ if theme != None:
         chosen = []
         for i in range(n):
             chosen.append(colors[round(i * n_distance)])
-        print(chosen)
 
         base = alt.Chart(pos)
 
-        for i, color in zip(range(len(x_pos_Rs)), chosen):
+        for i, color, angle in zip(range(len(x_pos_Rs)), chosen, theta_degs):
+            x_search_str = f"x_{angle}".replace(".", "․") # NOTE THIS IS REQUIRED - SECOND CHARACTER IS (U+2024) RATHER THAN (U+002E)
+            y_search_str = f"y_{angle}".replace(".", "․") # NOTE THIS IS REQUIRED - SECOND CHARACTER IS (U+2024) RATHER THAN (U+002E)
             if i == 0:
                 chart = alt.layer(
                     base.mark_point(color=color).encode(
-                        x=alt.X(f"x_pos_R_{i} / m", title="x / m"),
-                        y=alt.Y(f"y_pos_R_{i} / m", title="y / m"),
+                        x=alt.X(x_search_str, title="x / m"),
+                        y=alt.Y(y_search_str, title="y / m"),
                         strokeWidth=("t_R / s"),
                     )
                 )
             else:
                 chart += base.mark_point(color=color).encode(
-                    alt.X(f"x_pos_R_{i} / m"),
-                    alt.Y(f"y_pos_R_{i} / m"),
+                    alt.X(x_search_str),
+                    alt.Y(y_search_str),
                     strokeWidth=(f"t_R / s"),
                 )
 
         st.altair_chart(chart, use_container_width=True)
+        
+        st.markdown(f"###### p.s. the line for which the angle is not mentioned is always the first tag selected: {theta_degs[0]}°")
 
         st.subheader("Hover over each line for more info 📝")
