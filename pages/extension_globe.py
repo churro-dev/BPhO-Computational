eb8491e430b0c2f780f2c163de21d72ce9bcb36d
Sprocketer <90787028+Sprocketer@users.noreply.github.com>
2024-08-01 19:43:34 +0200

    Globe rendering

--- pages/extension_globe.py
+++ pages/extension_globe.py
@@ -2,12 +2,12 @@
 
 from math import cos, radians, sin, sqrt, tan, atan, degrees, pi
 
-import altair as alt
 import pandas as pd
 import streamlit as st
 from st_pages import add_indentation, show_pages_from_config
+from pyvista.examples import load_globe, load_globe_texture
 import pyvista as pv
-import stpyvista as stpv
+from stpyvista import stpyvista
 
 # Config page
 PAGE_TITLE = "Global Projectile Motion"
@@ -22,4 +22,13 @@ show_pages_from_config()
 
 # Main Content
 
-st.title("Global Projectile Motion")
+dataset = load_globe()
+texture = load_globe_texture()
+pl = pv.Plotter()
+pl.add_mesh(dataset, texture=texture)
+
+pl.camera_position = 'iso'
+pl.camera_focal_point = (0, 0, 0)
+pl.camera_view_up = (0, 1, 0)
+
+stpyvista(pl)
